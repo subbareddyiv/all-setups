5098c6135574cfd9d7cc3d883d8f9d71e36d4a65
subbareddyiv <subbareddyiv12@gmail.com>
2026-07-31 18:49:59 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,4 +2,4 @@ mkdir -p ~/.docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.39.1/docker-compose-linux-x86_64 \
 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-docker-compose version
+docker compose version
