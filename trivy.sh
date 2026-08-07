ebdc34cc3845cfde9cfbd353dc09cf87c0136f36
subbareddyiv <subbareddyiv12@gmail.com>
2026-08-07 23:59:44 +0530

    Create trivy.sh

---
+++ trivy.sh
@@ -0,0 +1,11 @@
+wget https://github.com/aquasecurity/trivy/releases/download/v0.73.0/trivy_0.73.0_Linux-32bit.tar.gz
+tar -zxvf trivy_0.73.0_Linux_32bit.tar.gz
+mv trivy /usr/local/bin/
+vim .bashrc
+export PATH=$PATH:/usr/local/bin/
+source .bashrc
+
+
+
+
+
